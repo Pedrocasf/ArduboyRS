826c95f2d54974e268055e86aa0a234a6f1c3515
Pedrocasf <pedrostarling2000@gmail.com>
2025-04-22 11:14:57 -0300

    Keep rethinking the interpreter.

--- avr-core/src/cpu/mod.rs
+++ avr-core/src/cpu/mod.rs
@@ -4,6 +4,7 @@ mod fuses;
 mod data_memory;
 mod io;
 mod exios;
+mod instructions;
 
 use alloc::{vec, vec::Vec};
 use kind::AVRKind;
@@ -12,12 +13,13 @@ use crate::cpu::fuses::Fuses;
 use crate::cpu::sreg::Flag;
 use sreg::Sreg;
 use crate::cpu::data_memory::DataMemory;
+use crate::cpu::instructions::InstructionData;
 use crate::cpu::kind::AVR_TYPE;
 
 pub struct CPU{
     fuses: Fuses,
     status: Sreg,
-    instr_array:[fn(&mut CPU);AVR_TYPE.flash_size as usize],
+    instr_array:[Option<(fn(&mut CPU), InstructionData)>;AVR_TYPE.flash_size as usize],
     data_memory: DataMemory,
     pc:u16,
 }
@@ -41,21 +43,33 @@ impl CPU{
         #[cfg(std)]
         println!("Instr:{:x} \n PC:{:x}",instr, pc);
     }
-    pub fn halt(&mut self)->u8{
+    pub fn halt(&mut self){
         panic!("HALT at {:x?}, instr:{:x?}", self.pc, self.instr_array[self.pc as usize])
+    }
+    pub fn jmp(&mut self){
+        let k = self.instr_array[self.pc as usize].1;
+        self.pc = k;
     }
 }
 pub fn translate(data:&[u16])->[fn(&mut CPU);AVR_TYPE.flash_size as usize]{
-    let mut r = Vec::new();
-    for d in data.iter().take(2){
+    let mut r = [CPU::halt; AVR_TYPE.flash_size as usize];
+    let mut data_peek_iter = data.iter().peekable();
+    let mut i = 0;
+    while data_peek_iter.peek().is_some() {
+        let d = data_peek_iter.next().unwrap();
         let op0 = ((d & 0xF000) >> 12) as u8;
         let op1 = ((d & 0x0F00) >> 08) as u8;
         let op2 = ((d & 0x00F0) >> 04) as u8;
         let op3 = ((d & 0x000F) >> 00) as u8;
         match (op0,op1,op2,op3) {
-            
+            (0x9,0x4,_,0xC) | (0x9,0x5,_,0xd)=>{
+                let k = data_peek_iter.next().unwrap();
+                r[i] = CPU::jmp;
+                i+=1
+            },
             (_,_,_,_) => panic!("Unsupported instruction, {:#x?}", d),
         }
+        i+=1;
     }
-    r.try_into().unwrap()
+    r
 }
